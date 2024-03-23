2a0885867018616758a5b8219534fd4fc541dd08
Manish201304 <107488431+Manish201304@users.noreply.github.com>
2024-03-24 01:59:59 +0530

    Hide apikey in .env.local file

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import LoadingBar from 'react-top-loading-bar'
 
 export default class App extends Component {
   pageSize = 15;
+  apiKey = process.env.REACT_APP_API_KEY
   generateKey = (category) => {
     return category.toLowerCase().replace(/\s/g, '') + '_news';
   }
@@ -31,13 +32,13 @@ export default class App extends Component {
             progress={this.state.progress}
           />
           <Route>
-            <Route exact path="/business"><News setProgress= {this.setProgress}key={this.generateKey("Business")} pageSize={this.pageSize} country="in" category="business" /></Route>
-            <Route exact path="/entertainment"><News setProgress={this.setProgress} key={this.generateKey("Entertainment")} pageSize={this.pageSize} country="in" category="entertainment" /></Route>
-            <Route exact path="/general"><News setProgress={this.setProgress} key={this.generateKey("General")} pageSize={this.pageSize} country="in" category="general" /></Route>
-            <Route exact path="/health"><News setProgress={this.setProgress} key={this.generateKey("Health")} pageSize={this.pageSize} country="in" category="health" /></Route>
-            <Route exact path="/science"><News setProgress={this.setProgress} key={this.generateKey("Science")} pageSize={this.pageSize} country="in" category="science" /></Route>
-            <Route exact path="/sports"><News setProgress={this.setProgress} key={this.generateKey("Sports")} pageSize={this.pageSize} country="in" category="sports" /></Route>
-            <Route exact path="/technology"><News setProgress={this.setProgress} key={this.generateKey("Technology")} pageSize={this.pageSize} country="in" category="technology" /></Route>
+            <Route exact path="/business"><News setProgress= {this.setProgress} apiKey={this.apiKey} key={this.generateKey("Business")} pageSize={this.pageSize} country="in" category="business" /></Route>
+            <Route exact path="/entertainment"><News setProgress={this.setProgress} apiKey={this.apiKey} key={this.generateKey("Entertainment")} pageSize={this.pageSize} country="in" category="entertainment" /></Route>
+            <Route exact path="/general"><News setProgress={this.setProgress} apiKey={this.apiKey} key={this.generateKey("General")} pageSize={this.pageSize} country="in" category="general" /></Route>
+            <Route exact path="/health"><News setProgress={this.setProgress} apiKey={this.apiKey} key={this.generateKey("Health")} pageSize={this.pageSize} country="in" category="health" /></Route>
+            <Route exact path="/science"><News setProgress={this.setProgress} apiKey={this.apiKey} key={this.generateKey("Science")} pageSize={this.pageSize} country="in" category="science" /></Route>
+            <Route exact path="/sports"><News setProgress={this.setProgress} apiKey={this.apiKey} key={this.generateKey("Sports")} pageSize={this.pageSize} country="in" category="sports" /></Route>
+            <Route exact path="/technology"><News setProgress={this.setProgress} apiKey={this.apiKey} key={this.generateKey("Technology")} pageSize={this.pageSize} country="in" category="technology" /></Route>
           </Route>
         </Router>
       </div>
